ac1c529c8152f54d43b473850b5980f304502ceb
zxysegh@gmail.com <20150130TUESDAy>
2017-04-20 22:51:06 +0800

    伪异步编程

---
+++ src/main/java/chaptersecond/paio/PseudoAsynchronousTest.java
@@ -0,0 +1,16 @@
+package chaptersecond.paio;
+
+import org.junit.Test;
+
+/**
+ * Created by XJX on 2017/4/20.
+ * <p>
+ * 伪异步编程
+ */
+public class PseudoAsynchronousTest {
+
+    @Test
+    public void server() {
+
+    }
+}
