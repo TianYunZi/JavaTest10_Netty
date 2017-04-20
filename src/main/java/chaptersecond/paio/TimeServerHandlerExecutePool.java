d4e546f42412a4f7367679e92e3337b7e994d7e8
zxysegh@gmail.com <20150130TUESDAy>
2017-04-20 23:06:18 +0800

    伪异步编程

---
+++ src/main/java/chaptersecond/paio/TimeServerHandlerExecutePool.java
@@ -0,0 +1,23 @@
+package chaptersecond.paio;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by XJX on 2017/4/20.
+ */
+public class TimeServerHandlerExecutePool {
+
+    private ExecutorService executorService;
+
+    public TimeServerHandlerExecutePool(int maxPoolSize, int queueSize) {
+        executorService = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), maxPoolSize, 120L, TimeUnit
+                .SECONDS, new ArrayBlockingQueue<>(queueSize));
+    }
+
+    public void execute(Runnable task) {
+        executorService.execute(task);
+    }
+}
